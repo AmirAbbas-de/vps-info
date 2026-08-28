8ebc879f3b27a4963d3fdee0b1dc2e8227ab161d
AmirAbbas-de <zohdiamirabbas5@gmail.com>
2026-08-28 10:34:32 +0330

    Ignore unwanted folders

--- OLD/FR/vps.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd /var/www/html/FR/ || exit
-source .venv/bin/activate
-echo "=== Run started at $(date '+%Y-%m-%d %H:%M:%S') ===" >> vps.get.log
-python3 vps.py >> vps.get.log 2>&1
-echo "=== Run finish
--- OLD/IR/vps.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd /var/www/html/PHP || exit
-source .venv/bin/activate
-echo "=== Run started at $(date '+%Y-%m-%d %H:%M:%S') ===" >> vps.get.log
-python3 vps.py >> vps.get.log 2>&1
-echo "=== Run finished at $(date '+%Y-%m-%d %H:%M:%S') ===" >> vps.get.log
-echo "" >> vps.get.log
